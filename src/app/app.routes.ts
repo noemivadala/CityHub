d66dae7c72df656321849faa1b54011a2ba0f224
noemivadala <noemi.vadala.nv@gmail.com>
2024-03-07 19:40:22 +0100

    sistemazione componenti pagine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    { path: 'home', loadComponent:() => import('./features/home/home.component') },
+    { path: 'home', canActivate: [authGuard], loadComponent:() => import('./features/home/home.component') },
     { path: 'users', loadComponent:() => import('./features/users/users.component') },
     { path: 'post', loadComponent:() => import('./features/post/post.component') },
-    { path: 'login', canActivate: [authGuard], loadComponent:() => import('./features/login/login.component') },
+    { path: 'login', loadComponent:() => import('./features/login/login.component') },
     { path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
