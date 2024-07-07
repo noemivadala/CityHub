499a166bdd75d391f167d87c4f3ee18c2ba8eba6
noemivadala <noemi.vadala.nv@gmail.com>
2024-07-07 19:01:03 +0200

    authguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
-    { path: 'users', /* canActivate: [authGuard],  */  loadComponent: () => import('./features/users/users.component')},
-    { path: 'post', loadComponent: () => import('./features/post/list-post.component')},
+    { path: 'users', canActivate: [AuthGuard],  loadComponent: () => import('./features/users/users.component')},
+    { path: 'post', canActivate: [AuthGuard], loadComponent: () => import('./features/post/list-post.component')},
     { path: 'login', loadComponent:() => import('./features/login/login.component')},
     { path: 'profile/:id', loadComponent:() => import('./features/users/profile-user.component') },
     { path: 'post/:postId', loadComponent:() => import('./features/comment/comment.component') },
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,26 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './service/auth.service';
+import { map, Observable, of } from 'rxjs';
 
-export const authGuard = () => {
-  const router = inject(Router)
-  const isLogged = true;
-  if (!isLogged) {
-    router.navigateByUrl('login')
+export class AuthGuard {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
+    const token = this.authService.getToken();
+    if (!token) {
+      this.router.navigate(['/login']);
+      return of(false);
+    }
+
+    return this.authService.validateToken(token).pipe(
+      map(isValid => {
+        if (!isValid) {
+          this.router.navigate(['/login']);
+        }
+        return isValid;
+      })
+    );
   }
-  return isLogged
 }
